aee0a76de304c5c67c0645b40962fdee359e73ac
Fabian Meyer <user8324@posteo.net>
2017-10-21 10:02:58 +0200

    Implement 4.a.

--- ex01/ex1_hanging_chain.m
+++ ex01/ex1_hanging_chain.m
@@ -5,21 +5,28 @@ close all;
 N = 40;
 
 % TODO: complete the definition of variables HERE
-y = ... ;
-z = ... ;
+y = sdpvar(N,1);
+z = sdpvar(N,1);
 
 m = 4/N;
 D = 70;
 g0 = 9.81;
 
 Vchain = 0;
+% define potential energy for springs
+for i = 1:N-1
+    Vchain = Vchain + 0.5 * D * ((y(i) - y(i+1))^2 + (z(i) - z(i+1))^2);
+end
+% define potential energy for masses
 for i = 1:N
-    % TODO: complete the objective function (i.e. potential energy) HERE
-    Vchain = Vchain + ...;
+    Vchain = Vchain + g0 * m * z(i);
 end
 
 % TODO: complete the (equality) constraints HERE
-constr = [  ... ];
+constr = [
+    [y(1) z(1)] == [-2 1];
+    [y(N) z(N)] == [ 2 1]
+];
 
 % Set options and solve the problem with quadprog:
 options = sdpsettings('solver', 'quadprog','verbose',2);
@@ -34,4 +41,3 @@ plot(-2,1,'xg','MarkerSize',10);
 plot(2,1,'xg','MarkerSize',10);
 xlabel('y'); ylabel('z');
 title('Optimal solution hanging chain (without extra constraints)')
-
