9311101ea52e4c0a0ffb1b1b0ee6dd8c3de9683a
Fabian Meyer <user8324@posteo.net>
2017-10-21 09:38:49 +0200

    Implement 3.c.

--- ex01/ex1_toy_example.m
+++ ex01/ex1_toy_example.m
@@ -1,29 +1,29 @@
 clc;
 clear all;
 close all;
-	
+
 % define variables
 x = sdpvar(1,1);
+y = sdpvar(1,1);
 
 % define objective
-f = x^2 - 2*x;
+f = x^2 - 2*x + y^2 + y;
 
 % define constraints
-C = [];
-%C = [C; x >= 1.5];
-
+C = [x >= 1.5; x + y >= 0];
 % define solver
 options = sdpsettings('solver', 'quadprog','verbose',2);
 
 % solve optimization problem
 diagn = optimize(C, f, options);
 
-% read and print solution
+% read variables as double 
 xopt = double(x);
+yopt = double(y);
 
 disp(' ')
 if diagn.problem == 0
-    disp(['Optimal solution found: x = ' num2str(xopt(1))]);
+    disp(['Optimal solution found: x = ' num2str(xopt(1)) ', y = ' num2str(yopt(1))]);
 else
     disp('Problem failed')
 end
