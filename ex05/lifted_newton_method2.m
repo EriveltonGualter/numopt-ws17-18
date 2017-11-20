da190386db2fdefbb89e92330caed7d6e85c0ac1
Fabian Meyer <user8324@posteo.net>
2017-11-20 11:38:42 +0100

    Implement ex05.3.c

---
+++ ex05/lifted_newton_method2.m
@@ -0,0 +1,40 @@
+% lifted_newton_method2.m
+%
+%     Author: Fabian Meyer
+% Created on: 20 Nov 2017
+
+clc;
+clear all;
+close all;
+
+x0 = 1 * [1 1 1 1]';
+
+% residual function
+F = @(x) [x(2) - x(1)^2;
+          x(3) - x(2)^2;
+          x(4) - x(3)^2;
+          -2   - x(4)^2];
+
+% Jacobian
+G = @(x) [-2*x(1)   1       0       0;
+           0       -2*x(2)  1       0;
+           0        0      -2*x(3)  1;
+           0        0       0      -2*x(4)];
+% transposed Jacobian  
+Gt = @(x) [-2*x(1)   0       0       0;
+            1       -2*x(2)  0       0;
+            0        1      -2*x(3)  0;
+            0        0       1      -2*x(4)];
+
+% calc root using newton method
+xk = newton_opt(x0, F, G);
+Fk = zeros(1,length(xk));
+for i = 1:length(Fk)
+    Fk(:,i) = norm(F(xk(:,i)));
+end
+length(xk)
+
+figure();
+title('Ex 05 Nr3c');
+plot(1:length(xk), Fk, 'DisplayName', 'foo');
+legend('show');
