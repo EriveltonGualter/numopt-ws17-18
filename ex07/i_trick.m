2fede7e6a64fd6f07369dad1f299c8b401e7fdd3
Fabian Meyer <user8324@posteo.net>
2017-12-12 08:57:22 +0100

    Implement FAD algorithm

--- ex07/i_trick.m
+++ ex07/i_trick.m
@@ -1,21 +1,21 @@
-function [F, J] = i_trick(f, x, param)
+function [F, J] = i_trick(f, U, param)
 % i_trick.m
 %
 %     Author: Fabian Meyer
 % Created on: 20 Nov 2017
 %
 % f: function handle of objective
-% x: point for Jacobian approx
+% U: point for Jacobian approx
 % param: parameters of function f
 % F: value of f at x
 % J: Jacobian approx of f at x
 
-p = ones(size(x,1)); % seed vector
+p = U; % seed vector
 t = eps; % t at machine precision
-xim = x + p * t * 1i; % complex input variable
+Uim = U + p * t * 1i; % complex input variable
 
-F = f(x, param);
-J = (imag(f(xim, param)) / t) + t^2;
+F = f(U, param);
+J = (imag(f(Uim, param)) / t) + t^2;
 
 end
 
