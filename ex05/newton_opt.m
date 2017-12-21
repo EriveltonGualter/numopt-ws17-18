7fb806e69a44cdbe3143313b1817e5c73008ff77
Fabian Meyer <user8324@posteo.net>
2017-12-21 14:37:10 +0100

    Rename some arguments

--- ex05/newton_opt.m
+++ ex05/newton_opt.m
@@ -1,7 +1,7 @@
-function [xk] = newton_opt(x0, G, B)
-% x0: Initial iterate to start netwon opt with, 1xn
-% G: gradient of objective function, function of x, 1xm
-% B: approx. hessian of objective function, function of x, mxm
+function [xk] = newton_opt(x0, F, J)
+% x0: Initial iterate to start netwon opt with, nx1
+% F: objective for root finding, function hnd, mx1
+% J: approx. Jacobian of F, function hnd, nxm
 % xk: iterates of optimization including x0
 
 % pre init result vector for fast computation
@@ -11,16 +11,16 @@ xk(:,1) = x0;
 
 for i = 1:N-1
     % calc current gradient and hessian
-    Bk = B(xk(:,i));
-    Gk = G(xk(:,i));
+    Jk = J(xk(:,i));
+    Fk = F(xk(:,i));
     
-    if norm(Gk) <= 1e-3
+    if norm(Fk) <= 1e-3
         xk = xk(:,1:i);
         break 
     end
     
     % calc newton step
-    pk = -inv(Bk) * Gk;
+    pk = -inv(Jk) * Fk;
     % calc new iterate
     xk(:, i+1) = xk(:,i) + pk;
 end
